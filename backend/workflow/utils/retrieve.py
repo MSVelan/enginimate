344f49a2648607430ace6c641806bd7417b1d86e
Muthiah Sivavelan <muthiahsvn@gmail.com>
2025-11-04 23:57:25 +0530

    added retrieval pipeline for docs filter by type

---
+++ backend/workflow/utils/retrieve.py
@@ -0,0 +1,77 @@
+async def retrieve_docs(user_query):
+    query = _get_detailed_instruct(user_query)
+    code_docs = await _retrieve_docs(query, k=3, type="code")
+    docstrings = await _retrieve_docs(query, k=2, type="documentation")
+    summary_docs = await _retrieve_docs(query, k=2, type="summary")
+    return code_docs + docstrings + summary_docs
+
+
+def _get_detailed_instruct(query: str) -> str:
+    """Instruct template for Qwen3-Embedding:0.6b embedding model"""
+    task_description = """
+Given a query, retrieve relevant documents that answer the query
+"""
+    return f"Instruct: {task_description}\nQuery:{query}"
+
+
+async def _get_pg_engine():
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+    CONNECTION_STRING = (
+        f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_HOST}"
+        f":{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
+
+    from langchain_postgres import PGEngine
+
+    pg_engine = PGEngine.from_connection_string(url=CONNECTION_STRING)
+    return pg_engine
+
+
+async def _retrieve_docs(query, k=3, type="code"):
+    """Retrieve documents of specific type for a given query.
+    Returns List[Tuple[Document: str, metadata: dict]]: list of Document objects
+    with similarity score.
+    """
+    from sqlalchemy import text
+
+    from langchain_core.documents import Document
+    from backend.workflow.utils.hf_space_wrapper import HFSpaceWrapper
+
+    hf_space = HFSpaceWrapper()
+
+    pg_engine = await _get_pg_engine()
+
+    query_embedding = hf_space.embed_query(query)
+    embedding_str = f"[{','.join(map(str, query_embedding))}]"
+
+    sql_query = """
+    select langchain_id, content, embedding, langchain_metadata,
+    (embedding <=> CAST(:query_embedding AS vector)) as distance
+    from public.manim_docs
+    where langchain_metadata->>'type'=:type
+    order by distance
+    limit :k;
+    """
+
+    async with pg_engine._pool.connect() as conn:
+        result = await conn.execute(
+            text(sql_query), {"query_embedding": embedding_str, "type": type, "k": k}
+        )
+        rows = result.fetchall()
+
+    docs = []
+    for row in rows:
+        doc = Document(page_content=row.content, metadata=row.langchain_metadata)
+        similarity_score = 1 - row.distance
+        docs.append((doc, similarity_score))
+
+    return docs
